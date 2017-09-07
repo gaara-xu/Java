e1e7506b42a5ed6955f18c17e1a6a248ebd403d9
gaara-xu <759499745@qq.com>
2017-09-07 10:16:38 +0800

    1.0.1

--- gril/src/main/java/com/src/GrilApplication.java
+++ gril/src/main/java/com/src/GrilApplication.java
@@ -2,9 +2,15 @@ package com.src;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class GrilApplication {
+
+/*	@Override
+	protected SpringApplicationBuilder springApplicationBuilder(SpringApplicationBuilder applicationBuilder){
+		return applicationBuilder.sources(GrilApplication.class);
+	}*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(GrilApplication.class, args);
